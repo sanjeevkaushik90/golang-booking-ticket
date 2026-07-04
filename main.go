839c30aca412905bc8c7cf49a4483e8c96283e40
Sanjeev Kaushik <sanjeevkaushik90684@gmail.com>
2026-07-04 16:46:35 +0530

    booking app

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name=" Oxford"
+	const seat=50
+	var remainSeat=50
+
+	fmt.Printf("Welcome to %s \n" ,name)
+	fmt.Println("Total seat are" ,seat , "and remainig seats are " , remainSeat)
+	fmt.Println("Book Your seat from here")
+
+}
